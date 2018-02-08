420e0393930e920a0e4def38ef4b7681f09ffdee
pepel <pepel@DESKTOP-VHT681R>
2018-02-08 19:02:46 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+
+    public class Program
+    {
+         private static void Main(string[] args)
+
+            {
+            Console.WriteLine(createmessage());
+            }
+
+            public static string createmessage()
+
+            {
+            return "Hello World";
+            }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld;
+
+namespace HelloWorldTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            Assert.AreEqual("Hello World", Program.createmessage());
+
+        }
+    }
+}
